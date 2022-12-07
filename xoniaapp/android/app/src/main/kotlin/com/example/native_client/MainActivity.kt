e1b9568510236ce850b537479c53fb19dde4710d
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-12-07 23:13:59 +0300

    project structure done

---
+++ xoniaapp/android/app/src/main/kotlin/com/example/native_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.xoniaapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ xoniaapp/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Xonia',
+      home: LandingPage(),
+    );
+  }
+}
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/logo.png'),
+      ),
+    );
+  }
+}
