1111657a99b0ef59047b2b93cbe986f5d6f89217
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-12-07 23:27:09 +0300

    fix broken color scheme code

--- xoniaapp/lib/color_scheme.dart
+++ xoniaapp/lib/color_scheme.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
-class ColorScheme {
-  final Color white = const Color(0xFFFFFFFF);
-  final Color darkOne = const Color(0xFF1B1C1D);
-  final Color darkTwo = const Color(0xFF131314);
-  final Color darkThree = const Color(0xFF000000);
-  final Color redOne = const Color(0xFFB53A3A);
-  final Color redTwo = const Color(0xFFBF3434);
-  final Color redThree = const Color(0xFFBD0D39);
-}
+const Color white = Color(0xFFFFFFFF);
+const Color darkOne = Color(0xFF1B1C1D);
+const Color darkTwo = Color(0xFF131314);
+const Color darkThree = Color(0xFF000000);
+const Color redOne = Color(0xFFB53A3A);
+const Color redTwo = Color(0xFFBF3434);
+const Color redThree = Color(0xFFBD0D39);
