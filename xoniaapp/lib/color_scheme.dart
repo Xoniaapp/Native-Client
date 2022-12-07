e4828500857813f0211e946a5c09210529855475
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-12-07 23:14:16 +0300

    add color scheme

---
+++ xoniaapp/lib/color_scheme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorScheme {
+  final Color white = const Color(0xFFFFFFFF);
+  final Color darkOne = const Color(0xFF1B1C1D);
+  final Color darkTwo = const Color(0xFF131314);
+  final Color darkThree = const Color(0xFF000000);
+  final Color redOne = const Color(0xFFB53A3A);
+  final Color redTwo = const Color(0xFFBF3434);
+  final Color redThree = const Color(0xFFBD0D39);
+}
