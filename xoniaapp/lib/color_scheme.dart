5c177509455b7fb162afe1fd3b77ef7fd146324b
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-12-08 13:31:49 +0300

    new colors in the color scheme

--- xoniaapp/lib/color_scheme.dart
+++ xoniaapp/lib/color_scheme.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 const Color white = Color(0xFFFFFFFF);
+const Color black = Color(0xFF000000);
+const Color gray = Color(0xFF4E4E4E);
 const Color darkOne = Color(0xFF1B1C1D);
 const Color darkTwo = Color(0xFF131314);
 const Color darkThree = Color(0xFF000000);
