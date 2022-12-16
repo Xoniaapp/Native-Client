12ee1bc71523faab84b6498e0cac6d135a177f92
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-12-16 18:43:57 +0300

    sign up page almost done

---
+++ xoniaapp/lib/pages/sign_up.dart
@@ -0,0 +1,295 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../color_scheme.dart' as color_scheme;
+import '../api_stuff.dart' as api_stuff;
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  bool _infoIsError = false;
+  String _info = '';
+  final Map _reqBody = {
+    'email': '',
+    'password': '',
+    'username': '',
+    'suffix': 6969
+  };
+
+  void _signUp() async {
+    setState(() {
+      _infoIsError = false;
+      _info = 'Signing up...';
+    });
+    var res = await http.post(api_stuff.uriForApiRoute('/auth/signup'),
+        body: jsonEncode(_reqBody),
+        headers: {'Content-Type': 'application/json'});
+    if (res.statusCode == 201) {
+      var decodedResponse = jsonDecode(utf8.decode(res.bodyBytes)) as Map;
+      print(decodedResponse);
+      return;
+    }
+    if (res.statusCode == 409) {
+      setState(() {
+        _infoIsError = true;
+        _info = 'Your username and suffix combination is already used';
+      });
+      return;
+    }
+    if (res.statusCode == 500) {
+      setState(() {
+        _infoIsError = true;
+        _info = 'Internal server error';
+      });
+      return;
+    }
+    setState(() => _info = '');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: color_scheme.darkOne,
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.only(top: 40),
+              child: Text('Sign up',
+                  style: TextStyle(color: color_scheme.white, fontSize: 20)),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: SizedBox(
+                width: 300,
+                child: TextField(
+                  onChanged: (input) {
+                    if (RegExp(
+                            r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$")
+                        .hasMatch(input)) {
+                      setState(() {
+                        _infoIsError = false;
+                        _info = '';
+                      });
+                      _reqBody['email'] = input;
+                    } else {
+                      setState(() {
+                        _infoIsError = true;
+                        _info = 'Email must be a valid email';
+                      });
+                    }
+                  },
+                  cursorColor: color_scheme.white,
+                  style: const TextStyle(color: color_scheme.white),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(10)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: color_scheme.redOne,
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(10)),
+                    hintText: 'Email',
+                    hintStyle: const TextStyle(
+                      color: color_scheme.gray,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 6),
+              child: SizedBox(
+                width: 300,
+                child: TextField(
+                  onChanged: (input) {
+                    if (input.length < 6) {
+                      setState(() {
+                        _infoIsError = true;
+                        _info =
+                            'Password length must be more than or equal to 6 characters';
+                      });
+                    } else if (input.length > 64) {
+                      setState(() {
+                        _infoIsError = true;
+                        _info =
+                            'Password length must be less than or equal to 64 characters';
+                      });
+                    } else {
+                      setState(() {
+                        _infoIsError = false;
+                        _info = '';
+                      });
+                      _reqBody['password'] = input;
+                    }
+                  },
+                  cursorColor: color_scheme.white,
+                  obscureText: true,
+                  style: const TextStyle(color: color_scheme.white),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(10)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: color_scheme.redOne,
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(10)),
+                    hintText: 'Password',
+                    hintStyle: const TextStyle(
+                      color: color_scheme.gray,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 6),
+              child: SizedBox(
+                width: 300,
+                child: TextField(
+                  onChanged: (input) {
+                    if (input.length < 2) {
+                      setState(() {
+                        _infoIsError = true;
+                        _info =
+                            'Username length must be more than or equal to 2 characters';
+                      });
+                    } else if (input.length > 32) {
+                      setState(() {
+                        _infoIsError = true;
+                        _info =
+                            'Username length must be less than or equal to 32 characters';
+                      });
+                    } else {
+                      setState(() {
+                        _infoIsError = false;
+                        _info = '';
+                      });
+                      _reqBody['username'] = input;
+                    }
+                  },
+                  cursorColor: color_scheme.white,
+                  style: const TextStyle(color: color_scheme.white),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(10)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: color_scheme.redOne,
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(10)),
+                    hintText: 'Username',
+                    hintStyle: const TextStyle(
+                      color: color_scheme.gray,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 6, bottom: 20),
+              child: SizedBox(
+                width: 300,
+                child: TextField(
+                  onChanged: (input) {
+                    int? suffixAsInt = int.tryParse(input);
+                    if (suffixAsInt == null) {
+                      setState(() {
+                        _infoIsError = true;
+                        _info = 'Tag must be a number';
+                      });
+                    } else if (input.length != 4) {
+                      setState(() {
+                        _infoIsError = true;
+                        _info = 'Tag must be 4 digits';
+                      });
+                    } else {
+                      setState(() {
+                        _infoIsError = false;
+                        _info = '';
+                      });
+                      _reqBody['suffix'] = suffixAsInt;
+                    }
+                  },
+                  cursorColor: color_scheme.white,
+                  style: const TextStyle(color: color_scheme.white),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(10)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: color_scheme.redOne,
+                          width: 2,
+                        ),
+                        borderRadius: BorderRadius.circular(10)),
+                    hintText: 'Tag',
+                    hintStyle: const TextStyle(
+                      color: color_scheme.gray,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: ElevatedButton(
+                        onPressed: _signUp,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: color_scheme.redOne,
+                        ),
+                        child: const Text('Sign up')),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => Navigator.pop(context),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: color_scheme.darkOne,
+                      side: const BorderSide(
+                        width: 2,
+                        color: color_scheme.redOne,
+                      ),
+                    ),
+                    child: const Text('Go back'),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 12, left: 12),
+              child: Text(_info,
+                  style: TextStyle(
+                      color: _infoIsError
+                          ? color_scheme.redOne
+                          : color_scheme.white,
+                      fontSize: 20)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
